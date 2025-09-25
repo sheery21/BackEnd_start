fdaedbda9cb35e6c1e8302210c3e4bf7cb845bef
sheery21 <sherypagal123@gmail.com.com>
2025-09-25 09:50:01 -0700

    fix: add server_2 as normal folder instead of submodule

---
+++ server_2/app.js
@@ -0,0 +1,47 @@
+import express from "express";
+import { product } from "./product.js";
+
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  console.log("request", req.url);
+  res.send('"Server Running!"');
+  console.log("Server Running!");
+});
+
+app.get("/about", (req, res) => {
+  res.send("About Page hello");
+});
+
+app.get("/contact", (req, res) => {
+  res.send("contact page");
+});
+
+app.get("/product", (req, res) => {
+  console.log("request", req.url);
+  res.send(product);
+});
+
+app.get("/product:id", (req, res) => {
+  console.log("request single prod api", req.url);
+  console.log("request params", typeof req.params.id);
+  const findProduct = product.find((obj) => {
+    console.log("obj", obj);
+
+    if (obj.id == req.params.id) {
+      return obj;
+    }
+  });
+  console.log("findProduct", findProduct);
+
+  if (findProduct) {
+    res.send(findProduct);
+  } else {
+    res.send("Product not Found");
+  }
+});
+
+app.listen(PORT, () =>
+  console.log(`✅ server running on http://localhost:${PORT}`)
+);
