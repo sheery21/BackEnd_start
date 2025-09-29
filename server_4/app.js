ac6eb77a580e3db01fd1f80a60b66c41a1d36ec7
sheery21 <sherypagal123@gmail.com.com>
2025-09-29 11:05:36 -0700

    practice Postman basics

--- server_4/app.js
+++ server_4/app.js
@@ -2,11 +2,26 @@ import express from "express";
 
 const app = express();
 const PORT = 3000;
+app.use(express.json())
+app.use(express.urlencoded({extended :true}))
 
+let userArr = [];
 
-app.get('/', (req, res) =>{
+app.post('/createuser',(req, res) =>{
+    userArr.push(req.body)
+    res.send('User Create Successfully')
+
+})
+app.get('/getuser', (req, res) =>{
     
-    res.send('server running')
+    res.send(userArr)
 });
+
+app.post('/deleteuser',(req, res) =>{
+    res.send('User Deleted Successfully')
+})
+app.post('/updateuser',(req, res) =>{
+    res.send('User Update Successfully')
+})
 
 app.listen(PORT, () => console.log( `server running on http://localhost${PORT}`));
